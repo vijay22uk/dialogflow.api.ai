eeb7bb427aa778ba88da861e14225ea876163001
vijay pandey <vpandey@merrillcorp.com>
2018-07-01 17:13:54 +0530

    decode msg

--- util/sock.js
+++ util/sock.js
@@ -1,6 +1,7 @@
 let io;
 let authActions = ["reports_done"];
 var api = require('./api');
+var jwt   = require('jsonwebtoken'); 
 var conn = function (app, server) {
     io = require('socket.io')(server);
 };
@@ -11,7 +12,15 @@ var fromClient = function () {
             api.getRes(data.client.data.text, socket.id).then(function (res) {
                 if (authActions.indexOf(res.action) !== -1 && !res.actionIncomplete) {
                     // Get data HERE
-                    socket.emit('fromServer', { server: 'Not Authorized', loginRequest: true });
+                    jwt.verify(token, process.env.SECRET, function (err, decoded) {
+                        if (err) {
+                            socket.emit('fromServer', { server: 'Not Authorized!!'});
+                        } else {
+                            //HEHE getProject(decoded.name)
+                            socket.emit('fromServer', { server: 'Wait::Running Query ' });
+                        }
+                    });
+                    // socket.emit('fromServer', { server: 'Not Authorized', loginRequest: true });
                 } else {
                     socket.emit('fromServer', { server: res.speech, action: res.action });
                 }
