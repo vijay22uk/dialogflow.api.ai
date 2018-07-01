f240fd536449adb682bb3176ee96187be1a177e1
vijay pandey <vpandey@merrillcorp.com>
2018-07-01 20:04:47 +0530

    landing page style changes

---
+++ src/Components/Common/features.js
@@ -0,0 +1,36 @@
+import React from 'react';
+export default function Features(props) {
+    return <div className="col-md-8">
+        <div className="wow fadeInRight animated" data-wow-delay="0.3s" style={{visibility: "visible",
+             animationDelay: '0.3s', animationName: "fadeInRight"}}>
+            <div className="features">
+                <i className="fa fa-check fa-2x circled bg-skin float-left animated tada "></i>
+                <h5>Modern interface</h5>
+                <p>
+                    Lorem ipsum dolor sit amet, nec te mollis utroque honestatis, ut utamur molestiae vix, graecis eligendi ne.
+                    Lorem ipsum dolor sit amet, nec te mollis utroque honestatis, ut utamur molestiae vix, graecis eligendi ne.
+                    Lorem ipsum dolor sit amet, nec te mollis utroque honestatis, ut utamur molestiae vix, graecis eligendi ne.
+        </p>
+        
+            </div>
+            <div className="features">
+                <i className="fa fa-check fa-2x circled bg-skin float-left animated tada "></i>
+                <h5>Easy to use</h5>
+                <p>
+                Lorem ipsum dolor sit amet, nec te mollis utroque honestatis, ut utamur molestiae vix, graecis eligendi ne.
+                    Lorem ipsum dolor sit amet, nec te mollis utroque honestatis, ut utamur molestiae vix, graecis eligendi ne.
+        </p>
+            </div>
+            <div className="features">
+                <i className="fa fa-check fa-2x circled bg-skin float-left animated tada "></i>
+                <h5>Real Time</h5>
+                <p>
+                Lorem ipsum dolor sit amet, nec te mollis utroque honestatis, ut utamur molestiae vix, graecis eligendi ne.
+                Lorem ipsum dolor sit amet, nec te mollis utroque honestatis, ut utamur molestiae vix, graecis eligendi ne.
+                Lorem ipsum dolor sit amet, nec te mollis utroque honestatis, ut utamur molestiae vix, graecis eligendi ne.
+                    Lorem ipsum dolor sit amet, nec te mollis utroque honestatis, ut utamur molestiae vix, graecis eligendi ne.
+        </p>
+            </div>
+        </div>
+    </div>;
+}
--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ApiCaller from '../../utils/api.caller';
+import Features from '../Common/features';
 import { Launcher } from 'react-chat-window';
 class Home extends Component {
     constructor(props) {
@@ -42,6 +43,7 @@ class Home extends Component {
 
 
             <div className="container container-fluid">
+<Features />
                 <Launcher
                 agentProfile={{
                     teamName: 'Live Reporting',
